ec9f6609ad9126cf7133bca8b140cdeac05ec91d
Dennis Hinzpeter <o0shojo0o@dietru.de>
2020-08-20 17:43:19 +0200

    Init

---
+++ index.js
@@ -0,0 +1,156 @@
+const axios = require('axios');
+const sqlite3 = require('sqlite3').verbose();
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const MessageAttachment = Discord.MessageAttachment;
+const MessageEmbed = Discord.MessageEmbed;
+const TOKEN = process.env.TOKEN;
+
+let db = new sqlite3.Database('./bot.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the bot database.');
+});
+
+db.all("select count(*) from ForumChangelog", [], (err, rows) => {
+    if (err) {
+        db.run('CREATE TABLE ForumChangelog(LastID number)');
+        console.log('Create ForumChangelog table.');
+    }
+});
+
+// https://discordapp.com/oauth2/authorize?&client_id=EURE-CLIENT-ID&scope=bot&permissions=8
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+        msg.channel.send('pong');
+    }
+
+    if (msg.content === 'what is my avatar') {
+        // Send the user's avatar URL
+        msg.reply(msg.author.displayAvatarURL());
+
+    }
+
+    if (msg.content === 'how to embed') {
+        const embed = new MessageEmbed()
+            .setTitle('A slick little embed')
+            .setColor(0xff0000)
+            .setDescription('Hello, this is a slick embed!');
+        msg.channel.send(embed);
+    }
+
+    if (msg.content === '!rip') {
+        const attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
+        msg.channel.send(attachment);
+    }
+
+    if (msg.guild) {
+        if (msg.content.startsWith('!kick')) {
+            const user = msg.mentions.users.first();
+            if (user) {
+                const member = msg.guild.member(user);
+                // If the member is in the guild
+                if (member) {
+                    member
+                        .kick('Optional reason that will display in the audit logs')
+                        .then(() => {
+                            // We let the message author know we were able to kick the person
+                            msg.reply(`Successfully kicked ${user.tag}`);
+                        })
+                        .catch(err => {
+                            msg.reply('I was unable to kick the member');
+                            // Log the error
+                            console.error(err);
+                        });
+                } else {
+                    // The mentioned user isn't in this guild
+                    msg.reply("That user isn't in this guild!");
+                }
+                // Otherwise, if no user was mentioned
+            } else {
+                msg.reply("You didn't mention the user to kick!");
+            }
+        }
+
+
+        if (msg.content.startsWith('!ban')) {
+            const user = msg.mentions.users.first();
+            // If we have a user mentioned
+            if (user) {
+                // Now we get the member from the user
+                const member = msg.guild.member(user);
+                // If the member is in the guild
+                if (member) {
+                    member
+                        .ban({
+                            reason: 'They were bad!',
+                        })
+                        .then(() => {
+                            // We let the message author know we were able to ban the person
+                            msg.reply(`Successfully banned ${user.tag}`);
+                        })
+                        .catch(err => {
+                            msg.reply('I was unable to ban the member');
+                            // Log the error
+                            console.error(err);
+                        });
+                } else {
+                    // The mentioned user isn't in this guild
+                    msg.reply("That user isn't in this guild!");
+                }
+            } else {
+                // Otherwise, if no user was mentioned
+                msg.reply("You didn't mention the user to ban!");
+            }
+        }
+    }
+});
+
+bot.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.cache.find(x => x.name === 'member-log');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Welcome to the server, ${member}`);
+});
+
+
+setInterval(BastelbunkerForumChangelogUpdateCheck, 1000);
+
+function BastelbunkerForumChangelogUpdateCheck() {
+    const _currentID = GetBastelbunkerForumChangelogLastID();
+    const _lastID = GetBastelbunkerForumChangelogLastIDFromDB();
+}
+
+function GetBastelbunkerForumChangelogLastID() {
+    var result = 0;
+    axios.get("https://forum.bastelbunker.de/api/discussions/21")
+        .then(response => {
+            const posts = response.data.data.relationships.posts.data;
+            result = posts[posts.length - 1].id;
+            console.log(result);
+        });
+    return result;
+}
+
+function GetBastelbunkerForumChangelogLastIDFromDB() {
+    var result = -1;
+    var huhu = db.all("select LastID from ForumChangelog", (rows) => {
+        return rows[0] ? rows[0].LastID : 0;
+    });
+
+    while (result === -1) { }
+
+    return result;
+}
