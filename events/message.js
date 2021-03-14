7bce6c7ad9e8dec2aae45ee89067492217f55146
Dennis Hinzpeter <o0shojo0o@dietru.de>
2021-03-14 12:06:57 +0100

    Update

--- events/message.js
+++ events/message.js
@@ -1,15 +1,21 @@
 const prefix = "!"
 
 module.exports.run = async (client, message) => {
-    if (message.author.bot) return;
+    // Is the bot itself the author of the incoming message?
+    if (message.author.bot) {
+        return;
+    }
+    // Starts the incoming message with the prefix?
     if (message.content.startsWith(prefix)) {
 
-        let messageArray = message.content.split(" "),
-            cmd = messageArray[0],
-            args = messageArray.slice(1),
-            commandfile = client.commands.get(cmd.slice(prefix.length)) || client.aliases.get(cmd.slice(prefix.length));
+        const messageArray = message.content.split(' ');
+        const cmd = messageArray[0];
+        const args = messageArray.slice(1);
+        const commandfile = client.commands.get(cmd.slice(prefix.length)) || client.aliases.get(cmd.slice(prefix.length));
 
-        if (!commandfile) return;
+        if (!commandfile){
+            return;
+        } 
         commandfile.execute(client, message, args);
     }
 }
